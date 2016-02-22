366742c9d2a951343de814e716774af742bae20b
dadiletta <dan.adiletta@gmail.com>
2016-02-22 11:08:45 -0500

    Ready for practice. Still needs right motor modifier

--- src/com/myteam/robot/Robot.java
+++ src/com/myteam/robot/Robot.java
@@ -1,6 +1,24 @@
-/* Created Thu Jan 28 17:15:33 EST 2016 */
+/*
+ * Team 5973 - Moonshots - FRC Stronghold 2016
+ * BASED ON STRONGBACK
+ * Copyright 2015, Strongback and individual contributors by the @authors tag.
+ * See the COPYRIGHT.txt in the distribution for a full listing of individual
+ * contributors.
+ *
+ * Licensed under the MIT License; you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://opensource.org/licenses/MIT
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.myteam.robot;
+
+import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
 
 import org.strongback.Strongback;
 import org.strongback.SwitchReactor;
@@ -8,6 +26,7 @@ import org.strongback.components.Motor;
 import org.strongback.components.ui.ContinuousRange;
 import org.strongback.components.ui.FlightStick;
 import org.strongback.drive.TankDrive;
+import org.strongback.function.DoubleToDoubleFunction;
 import org.strongback.hardware.Hardware;
 import org.strongback.util.Values;
 
@@ -19,41 +38,50 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 public class Robot extends IterativeRobot {
 	
     private static final int JOYSTICK_PORT = 0; // in driver station
-    private static final int LMOTOR_PORT = 2;
-    private static final int RMOTOR_PORT = 5; //was switched from port 1 to troubleshoot
+    private static final int LMOTOR_PORT = 5;
+    private static final int RMOTOR_PORT = 2; 
     private static final int RARM_PORT = 3; 
     private static final int LARM_PORT = 4;
+    private static final double ARM_UP_LIMIT = 0.3;
+    private static final double ARM_DOWN_LIMIT = 0.7;
     private TankDrive drive;
     private ContinuousRange driveSpeed;
     private ContinuousRange turnSpeed;
     
-    //Used to limit the number of console outputs
-    public int filter = 0;
+    //Used to limit and format the number of console outputs
+    private int filter = 0;
+	private String pattern = "###.###";
+	private DecimalFormat myFormat = new DecimalFormat(pattern);
+	private double sen;
     
     
-    
-    //We moved this up here so we can 
+    //We moved this up here so we can output this variable in the teleop
     protected ContinuousRange sensitivity;
-    
-    //trying to move this joystick init outisde of robotInit so telopPeriodic can access stick control
     
     
     @Override
     public void robotInit() {
-    	Strongback.configure().recordNoEvents().recordNoData().initialize();
- 
-        Motor left = Hardware.Motors.victorSP(LMOTOR_PORT);
-        Motor right = Hardware.Motors.victorSP(RMOTOR_PORT).invert();
+    	//Execution period extended so it doesn't time out per cycle
+    	//Records no data and no events, a feature of Strongback we will look at next year
+    	Strongback.configure().recordNoData().recordNoCommands().recordNoEvents()
+    		.useExecutionPeriod(300, TimeUnit.MILLISECONDS).initialize();
+        
+    	Motor left = Hardware.Motors.victorSP(LMOTOR_PORT).invert();
+    	DoubleToDoubleFunction SPEED_LIMITER = Values.limiter(-0.1, 0.1);
+    	Motor right = Hardware.Motors.victorSP(RMOTOR_PORT, SPEED_LIMITER);
         drive = new TankDrive(left, right); 
         
         Motor armLeft = Hardware.Motors.victorSP(LARM_PORT);
         Motor armRight = Hardware.Motors.victorSP(RARM_PORT);
+        //Arms combined so they always move in sync
         Motor arm = Motor.compose(armLeft, armRight);
         
         
-        //CAMERA NIGHTMARE
+        //FIRST CAMERA ATTEMPT:
         //USBCamera cam0 = new USBCamera();
         //cam0.startCapture();
+        
+        //Camera must be in the outside USB port. Check Rio dashboard for the name of the camera
         CameraServer camera = CameraServer.getInstance();
         camera.setQuality(25);
         camera.startAutomaticCapture("cam1");
@@ -67,15 +95,16 @@ public class Robot extends IterativeRobot {
         // ContinuousRange sensitivity = joystick.getThrottle().map(t -> ((t + 1.0) / 2.0));
         sensitivity = joystick.getThrottle().map(Values.mapRange(-1.0,1.0).toRange(0.0, 1.0));
         driveSpeed = joystick.getPitch().scale(sensitivity::read); // scaled
-        turnSpeed = joystick.getRoll().scale(sensitivity::read).invert(); // scaled and inverted
+        turnSpeed = joystick.getRoll().scale(sensitivity::read); // scaled and inverted
         
         
-        reactor.onTriggered(joystick.getTrigger(), () -> arm.setSpeed(-sensitivity.read()));
+        //Triggers get only .7 of our sensitivity
+        reactor.onTriggered(joystick.getTrigger(), () -> arm.setSpeed(-sensitivity.read()*ARM_DOWN_LIMIT));
         reactor.onUntriggered(joystick.getTrigger(), () -> arm.stop());
         //limit switches here are very important and needed
         //We'd add physical triggers that would stop the arm from over extending
         //the motor's encoders COULD be used but are tricky as they reset every time
-        reactor.onTriggered(joystick.getThumb(), () -> arm.setSpeed(sensitivity.read()));
+        reactor.onTriggered(joystick.getThumb(), () -> arm.setSpeed(sensitivity.read()*ARM_UP_LIMIT));
         reactor.onUntriggered(joystick.getThumb(), () -> arm.stop());     
        
     }
@@ -85,8 +114,7 @@ public class Robot extends IterativeRobot {
         //Start Strongback functions ...
         Strongback.start();
         
-        //this timedDrive class is in the package but it gets lost. Move the package
-        //information around on the class and it will load right away
+        //Moves forward for 5 seconds
         Strongback.submit(new TimedDriveCommand(drive, -0.5, 0.0, false, 5.0));
     }
     
@@ -103,9 +131,13 @@ public class Robot extends IterativeRobot {
     public void teleopPeriodic() {
         drive.arcade(driveSpeed.read(), turnSpeed.read()); 
         
-        //Checking on the drive speed value every 10 cycles
-        if(filter % 20 == 0)
-        	System.out.println("Sensitivity: " + sensitivity.read());
+        //Prints the sensitivity value every 40 cycles
+        if(filter % 40 == 0){
+        	sen = sensitivity.read();
+        	System.out.println("Sensitivity: " + myFormat.format(sen) 
+        			+ " // arm UP: " + myFormat.format(sen*ARM_UP_LIMIT)
+        			+ " // arm DOWN: " + myFormat.format(sen*ARM_DOWN_LIMIT));
+        }
         filter++;
     }
 
